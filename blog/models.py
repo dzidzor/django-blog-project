278fa1a35139d73f9e155d6adddbaba5f19b7082
AITI Student <aiti-student@aiti-093-PC.(none)>
2012-07-06 12:07:57 -0400

    update

--- blog/models.py
+++ blog/models.py
@@ -1,20 +1,48 @@
 from django.db import models
 from django.contrib import admin
+#from myproject.myapp.models import Author
 
 # Create your models here.
 class Post(models.Model):
-    title=models.CharField(max_length=60)
-    body=models.CharField(max_length=300)
-    created=models.DateField()
-    updated=models.DateField()
+    title = models.CharField(max_length=60)
+    body = models.TextField()
+    created = models.DateField(auto_now=True)
+    updated = models.DateField(auto_now=True)
+    def __unicode__ (self):
+        return self.title
 
 class Comment(models.Model):
-    body=models.CharField(max_length=300)
+    body=models.TextField()
     author=models.CharField(max_length=60)
-    created=models.DateField()
-    update=models.DateField()
+    created=models.DateField(auto_now=True)
+    update=models.DateField(auto_now=True)
     post=models.ForeignKey(Post)
+    def __unicode__ (self):
+        return self.author
+    def first_60(self):
+        return self.title[:60]
 
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=('title','created','updated')
+    search_fields=('title','body')
+    list_filter=('created')
+    
+    
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author','first_60','date created','date updated')
+    list_filter=('created','author')
+    
+    
+    
+
+class CommentInline(admin.TabularInline):
+    model=Comment
+class AuthorAdmin(admin.ModelAdmin):
+    inlines=[CommentInline]
+    
 admin.site.register(Post)
 admin.site.register(Comment)
-
+  
+    
--- dzi_blog/settings.py
+++ dzi_blog/settings.py
@@ -12,14 +12,14 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': dj_database_url.config(default="sqlite:/blog_dev.db")
 }
-{
+"""{
         'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    }"""
 
 
 # Local time zone for this installation. Choices can be found here:
